235b3fedadf2c926119c371e59c1ff93c5571d28
hao.cong <hao.cong@app2m.com>
2017-10-29 00:45:52 +0800

    upgrade to android 3.0.0

--- samples/src/main/java/com/app2m/samples/SampleAlbumActivity.java
+++ samples/src/main/java/com/app2m/samples/SampleAlbumActivity.java
@@ -1,5 +1,7 @@
 package com.app2m.samples;
 
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,15 +9,17 @@ import android.widget.Toast;
 
 import com.app2m.album.AlbumUtil;
 import com.app2m.album.bean.MediaFolderBean;
+import com.app2m.samples.databinding.ActivitySampleAlbumBinding;
 
 import java.util.List;
 
 public class SampleAlbumActivity extends AppCompatActivity {
-
+    private ActivitySampleAlbumBinding mBinding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sample_album);
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_sample_album);
+        mBinding.setActivity(this);
     }
 
     public void onClickAlbumUtilGetAllMediasByFolder(View view) {
@@ -51,5 +55,4 @@ public class SampleAlbumActivity extends AppCompatActivity {
             Toast.makeText(this, "nothing!", Toast.LENGTH_SHORT).show();
         }
     }
-
 }
